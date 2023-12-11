79f6cc4bbac61648428543ba49c715278d9f46d1
Edward Tan <guofoo@gmail.com>
2023-12-11 15:50:59 -0800

    updates to use latest Makepad refactor's MatchEvent() and action handling

--- counter/src/app.rs
+++ counter/src/app.rs
@@ -29,7 +29,6 @@ live_design! {
             height: Fill
             
             draw_bg: {
-                
                 // The `fn pixel(self) -> vec4` syntax is used to define a property named `pixel`,
                 // the value of which is a shader. We use our own custom DSL to define shaders. It's
                 // syntax is *mostly* compatible with GLSL, although there are some differences as
@@ -43,7 +42,8 @@ live_design! {
             }
             
             // The `name:` syntax is used to define fields, i.e. properties for which there are
-            // corresponding struct fields. In contrast, the `name =` syntax is used to define
+            // corresponding struct fields.
+            // In contrast, the `name =` syntax is used to define
             // instance properties, i.e. properties for which there are no corresponding struct
             // fields. Note that fields and instance properties use different namespaces, so you
             // can have both a field and an instance property with the same name.
@@ -80,7 +80,6 @@ live_design! {
                 // tells the Makepad runtime our DSL object has the same properties as the DSL object
                 // named `Button`, except for the properties defined here below, which override any
                 // existing values.
-                
                 button1 = <Button> {
                     text: "Count"
                 }
@@ -103,9 +102,9 @@ app_main!(App);
 // The #[derive(Live, LiveHook)] attribute implements a bunch of traits for this struct that enable
 // it to interact with the Makepad runtime. Among other things, this enables the Makepad runtime to
 // initialize the struct from a DSL object.
-#[derive(Live)]
+#[derive(Live, LiveHook)]
 // This function is used to register any DSL code that you depend on.
-// called automatically by the code we generated with the call to the macro `main_app` above.
+// called automatically by the code we generated with the call to the macro `app_main` above.
 
 pub struct App {
     // A chromeless window for our application. Used to contain our view widget.
@@ -119,48 +118,28 @@ pub struct App {
     #[rust] counter: usize,
 }
 
-impl LiveHook for App {
-    fn before_live_design(cx: &mut Cx) {
+impl LiveRegister for App {
+    fn live_register(cx: &mut Cx) {
         crate::makepad_widgets::live_design(cx);
     }
 }
 
-impl App {
-    async fn _do_network_request(_cx:CxRef, _ui:WidgetRef, _url:&str)->String{
-        //let x = fetch(urL).await;
-        //ui.get_label(id!(thing)).set_text(&mut *cx.borrow_mut(), x);
-        "".to_string()
+impl MatchEvent for App{
+    fn handle_actions(&mut self, cx: &mut Cx, actions:&Actions){
+        if self.ui.button(id!(button1)).clicked(&actions) {
+            self.counter += 1;
+            log!("BUTTON CLICKED {}", self.counter); 
+            let label = self.ui.label(id!(label1));
+            label.set_text_and_redraw(cx, &format!("{}", self.counter));
+        }
     }
 }
 
 impl AppMain for App{
-    
     // This function is used to handle any incoming events from the host system. It is called
-    // automatically by the code we generated with the call to the macro `main_app` above.
+    // automatically by the code we generated with the call to the macro `app_main` above.
     fn handle_event(&mut self, cx: &mut Cx, event: &Event) {
-        let mut scope =  WidgetScope::default();
-
-        if let Event::Draw(event) = event {
-            // This is a draw event, so create a draw context and use that to draw our application.
-            return self.ui.draw_all(&mut Cx2d::new(cx, event), &mut scope);
-        }
-        
-        // Forward the event to the view. In this case, handle_event returns a list of actions.
-        // Actions are similar to events, except that events are always forwarded downward to child
-        // widgets, while actions are always returned back upwards to parent widgets.
-        let actions = self.ui.handle_event(cx, event, &mut scope);
-        
-        // Get a reference to our button from the view, and check if one of the actions returned by
-        // the view was a notification that the button was clicked.
-        if self.ui.button(id!(button1)).clicked(&actions) {
-            // Increment the counter.
-            self.counter += 1;
-            
-            // Get a reference to our label from the view, update its text, and schedule a redraw
-            // for it.
-            let label = self.ui.label(id!(label1));
-            label.set_text(&format!("{}", self.counter));
-            label.redraw(cx);
-        }
+        self.match_event(cx, event);
+        self.ui.handle_event(cx, event, &mut Scope::empty());
     }
 }
--- temp_conv/src/app.rs
+++ temp_conv/src/app.rs
@@ -82,7 +82,7 @@ app_main!(App);
 // The #[derive(Live, LiveHook)] attribute implements a bunch of traits for this struct that enable
 // it to interact with the Makepad runtime. Among other things, this enables the Makepad runtime to
 // initialize the struct from a DSL object.
-#[derive(Live)]
+#[derive(Live, LiveHook)]
 
 pub struct App {
     // A frame widget. Used to contain our button and label.
@@ -96,33 +96,14 @@ pub struct App {
     #[rust] f_value: String,
 }
 
-impl LiveHook for App {
-    fn before_live_design(cx: &mut Cx) {
+impl LiveRegister for App {
+    fn live_register(cx: &mut Cx) {
         crate::makepad_widgets::live_design(cx);
-    }
+    } 
 }
 
-impl App {
-    // pub fn live_design(cx: &mut Cx) {
-    //     makepad_widgets::live_design(cx);
-    // }
-}
-
-impl AppMain for App{
-
-    fn handle_event(&mut self, cx: &mut Cx, event: &Event) {
-        let mut scope =  WidgetScope::default();
-        
-        if let Event::Draw(event) = event {
-            // This is a draw event, so create a draw context and use that to draw our application.
-            return self.ui.draw_all(&mut Cx2d::new(cx, event), &mut scope);
-        }
-
-        // Forward the event to the frame. In this case, handle_event returns a list of actions.
-        // Actions are similar to events, except that events are always forwarded downward to child
-        // widgets, while actions are always returned back upwards to parent widgets.
-        let actions = self.ui.handle_event(cx, event, &mut scope);
-
+impl MatchEvent for App {
+    fn handle_actions(&mut self, cx:&mut Cx, actions:&Actions){
         let res = self.ui.text_input(id!(input_celsius)).changed(&actions);
         match res {
             Some(s) => match s.parse::<i32>() {
@@ -130,8 +111,7 @@ impl AppMain for App{
                     self.f_value = (number * 9/5 + 32).to_string();
                     println!("F={}", self.f_value);
                     let inp_f = self.ui.text_input(id!(input_fahrenheit));
-                    inp_f.set_text(&format!("{}", self.f_value));
-                    inp_f.redraw(cx);
+                    inp_f.set_text_and_redraw(cx, &format!("{}", self.f_value));
                 }
                 Err(e) => {
                     println!("Invalid input. Please enter an integer. {}", e);
@@ -141,7 +121,6 @@ impl AppMain for App{
                 // println!("No input to parse")
             }
         }
-
         let res = self.ui.text_input(id!(input_fahrenheit)).changed(&actions);
         match res {
             Some(s) => match s.parse::<i32>() {
@@ -149,8 +128,7 @@ impl AppMain for App{
                     self.c_value = ((number - 32) * 5/9).to_string();
                     println!("C={}",  self.c_value);
                     let inp_c = self.ui.text_input(id!(input_celsius));
-                    inp_c.set_text(&format!("{}", self.c_value));
-                    inp_c.redraw(cx);
+                    inp_c.set_text_and_redraw(cx, &format!("{}", self.c_value));
                 }
                 Err(e) => {
                     println!("Invalid input. Please enter an integer. {}", e);
@@ -158,6 +136,14 @@ impl AppMain for App{
             },
             None => {
                 // println!("No input to parse")
-            }        }
+            }
+        }
+    }
+}
+
+impl AppMain for App {
+    fn handle_event(&mut self, cx: &mut Cx, event: &Event) {
+        self.match_event(cx, event);
+        self.ui.handle_event(cx, event, &mut Scope::empty());
     }
 }
